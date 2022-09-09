b25a62139b2b2f8464c52744299044866ffcb84a
BlinorKajtaziDigit <107686780+BlinorKajtaziDigit@users.noreply.github.com>
2022-09-09 16:06:45 +0200

    bug fixes

--- src/components/UI/HeroSlider.jsx
+++ src/components/UI/HeroSlider.jsx
@@ -19,7 +19,7 @@ const HeroSlider = () => {
   };
   return (
     <Slider {...settings} className="hero__slider">
-      <div className="slider__item slider__item-01 mt0">
+      <div className="slider__item slider__item-03 mt0">
         <Container>
           <div className="slider__content ">
             <h1 className="text-light mb-4">CONTEXT</h1>
@@ -35,20 +35,20 @@ const HeroSlider = () => {
         </Container>
       </div>
 
-      {/* <div className="slider__item slider__item-02 mt0">
+      <div className="slider__item slider__item-02 mt0">
         <Container>
           <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-            <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
+            <h4 className="text-light mb-3">CONTEXT</h4>
+            <h1 className="text-light mb-4">Systematisch. Methodisch. Effizient.</h1>
 
             <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+              <Link to="/cars"> Portofolio</Link>
             </button>
           </div>
         </Container>
       </div>
 
-      <div className="slider__item slider__item-03 mt0">
+      {/* <div className="slider__item slider__item-03 mt0">
         <Container>
           <div className="slider__content ">
             <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
