d4f82ad9298e0f99762fbe64806910d0cecce03d
EuronRamadanii <e.ramadani@digitbusiness.ch>
2022-09-09 14:10:33 +0200

    Footer content fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -41,9 +41,10 @@ const Footer = () => {
             <div className="logo footer__logo">
               <h1>
                 <Link to="/home" className=" d-flex align-items-center gap-2">
-                  <i class="ri-car-line"></i>
+                  {/* <i class="ri-car-line"></i> */}
                   <span>
-                    Rent Car <br /> Service
+                    {/* Rent Car <br /> Service */}
+                    Context
                   </span>
                 </Link>
               </h1>
@@ -72,10 +73,12 @@ const Footer = () => {
           <Col lg="3" md="4" sm="6">
             <div className="mb-4">
               <h5 className="footer__link-title mb-4">Head Office</h5>
-              <p className="office__info">123 Zindabazar, Sylhet, Bangladesh</p>
-              <p className="office__info">Phone: +0995345875365</p>
+              <p className="office__info">
+                Bremer Str. 11 80807 München Deutschland
+              </p>
+              <p className="office__info">Phone: +49 (0)89 35477460</p>
 
-              <p className="office__info">Email: muhib5532@gmail.com</p>
+              <p className="office__info">Email: Lorem2@gmail.com</p>
 
               <p className="office__info">Office Time: 10am - 7pm</p>
             </div>
@@ -98,7 +101,7 @@ const Footer = () => {
             <div className="footer__bottom">
               <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
                 <i class="ri-copyright-line"></i>Copyright {year}, Developed by
-                Muhibur Rahman. All rights reserved.
+                DigIT. All rights reserved.
               </p>
             </div>
           </Col>
